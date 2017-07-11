79b2f9f073a05326e839db3d9cbbc1cf9af87537
Dkner <appljuic@hotmail.com>
2017-07-11 17:58:39 +0800

    pic

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,5 +1,4 @@
 import os
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
@@ -8,7 +7,6 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    # DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='key',
     USERNAME='liliang',
     PASSWORD='rock'
@@ -18,15 +16,18 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 entries = [
     {
         'title': 1,
-        'text': 'liliang1'
+        'text': 'liliang1',
+        'img': 'static/img/Koala.jpg'
     },
     {
         'title': 2,
-        'text': 'liliang2'
+        'text': 'liliang2',
+        'img': 'static/img/Koala.jpg'
     },
     {
         'title': 3,
-        'text': 'liliang3'
+        'text': 'liliang3',
+        'img': 'static/img/Koala.jpg'
     }
 ]
 
