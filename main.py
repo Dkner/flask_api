94428112f1f6838c103e511fcf5687df338a80c4
Dkner <appljuic@hotmail.com>
2017-07-24 17:55:46 +0800

    test

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
-@app.route('/todo/api/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     from werkzeug.contrib.fixers import ProxyFix
     app.wsgi_app = ProxyFix(app.wsgi_app)
-    app.run(debug=True)
+    app.run()
